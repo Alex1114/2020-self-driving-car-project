6a2b4c63f58cbebb32afb5249ab7e77bbda42e7a
alex1114 <pokai.eed05@g2.nctu.edu.tw>
2020-05-06 14:19:08 +0800

    Nuscenes

--- download_Nuscenes.py
+++ download_Nuscenes.py
@@ -4,7 +4,7 @@ import logging
 import gdown
 from zipfile import ZipFile
 
-dataset_url = ''
+dataset_url = 'https://drive.google.com/u/1/uc?id=1Y5ietDYpmXSJXaqvFg3BCb3p_-Q_HlRp&export=download'
 dataset_name = 'data'
 
 gdown.download(dataset_url, output=dataset_name + '.zip', quiet=False)
