086147d5d142230c76e4e1016ed9f3ed9846f6ca
huangjuite <huangjuite@gmail.com>
2020-05-27 21:05:48 +0800

    script

--- Tracking/argoverse_cbgs_kf_tracker/run_ab3d.sh
+++ Tracking/argoverse_cbgs_kf_tracker/run_ab3d.sh
@@ -1,5 +1,5 @@
 #! /bin/bash
 python3 run_ab3dmot.py \
     --dets_dataroot /home/arg/argoai/argoverse_detections_2020/ \
-    --pose_dir /home/arg/argoai/argodataset/tracking/test/test/ \
+    --pose_dir /home/arg/argoai/argodataset/tracking/test/ \
     --split test
